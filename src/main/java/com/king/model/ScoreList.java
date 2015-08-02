7e7a705ee71726ecc64e673818f40b87562584b3
freyliis <ania.kurman@gmail.com>
2015-08-02 11:52:00 +0200

    Wrong place for synchronization for scorelist

--- src/main/java/com/king/model/ScoreList.java
+++ src/main/java/com/king/model/ScoreList.java
@@ -2,7 +2,10 @@ package com.king.model;
 
 import com.king.model.comparator.highscore.HighScoreComparator;
 
-import java.util.*;
+import java.util.List;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
 import java.util.concurrent.ConcurrentSkipListSet;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
@@ -16,14 +19,14 @@ public class ScoreList {
     private HighScoreComparator highScoreComparator;
 
     public ScoreList(List<Score> scoreList, int highScoreListSize, HighScoreComparator highScoreComparator) {
-        this.scoreSet = new TreeSet<Score>(highScoreComparator);
+        this.scoreSet = new ConcurrentSkipListSet<Score>(highScoreComparator);
         this.scoreSet.addAll(scoreList);
         this.highScoreListSize = highScoreListSize;
         this.highScoreComparator = highScoreComparator;
     }
 
     public Set<Score> getHighScoreList() {
-        Supplier<SortedSet<Score>> supplier = () -> new ConcurrentSkipListSet<>(highScoreComparator);
+        Supplier<SortedSet<Score>> supplier = () -> new TreeSet<Score>(highScoreComparator);
         Set<Score> scores = scoreSet.stream().limit(highScoreListSize).collect(Collectors.toCollection(supplier));
         return scores;
     }
